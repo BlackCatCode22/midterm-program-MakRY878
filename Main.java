a24152a9418c017c20c453769e0bf7fef5ff45c3
MakRY878 <141769737+MakRY878@users.noreply.github.com>
2023-10-18 17:23:15 -0700

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,62 +1,167 @@
-
-//updated 9/28
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Date;
+import java.util.Calendar;
+import java.time.LocalDate;
 
 public class Main {
 
     // Creating the genUniqueID method
 
-    private static String genUniqueID(String theSpecies, int numOfSpecies){
+
+    public static String genUniqueID(String theSpecies, int numOfSpecies) {
         String prefix = "";
         int suffix = numOfSpecies + 1;
-        if(theSpecies.contains("hyena")){
+        if (theSpecies.contains("hyena")) {
             prefix = "Hy";
+        } else if (theSpecies.contains("lion")) {
+            prefix = "Li";
+
+        } else if (theSpecies.contains("bear")) {
+            prefix = "Be";
+
+        } else if (theSpecies.contains("tiger")) {
+            prefix = "Ti";
+
+        } else {
+            prefix = "XX";
         }
-        return prefix + Integer.valueOf(suffix);
+
+        return prefix + suffix;
 
 
     }
+
+
+
+    // Call the genUniqueID method and store the result in a variable
+    // genUniqueID(theSpecies, numOfSpecies);
+
+    // Print the generated unique ID
+
+
+
 
     public static void main(String[] args) {
 
-        //call the static method to create a list of names
-
-
         System.out.println("\n Welcome to the zoo.");
         //look at our animal names file
-        //call inputHyenaNames()
+        //call the static method to create a list of names
 
-       // Hyena.inputHyenaNames();
+        Hyena.inputHyenaNames();
+        Bear.inputBearNames();
+        Lion.inputLionNames();
+        Tiger.inputTigerNames();
+
+        Hyena.listOut();
+        Bear.listOut();
+        Lion.listOut();
+        Tiger.listOut();
+
+        //open a csv file using the split() method on a string object
+
+        String path = "C:/Users/Makal/OneDrive/Documents/zooProgramFile/arrivingAnimals.txt";
+        String myFileLine= "";
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(path));
+            int myCounter = 1;
+            while ((myFileLine = reader.readLine()) != null){
+                //the input data from arriving animals should look like this
+                // 4 year old female hyena, born in spring, tan color, 70 pounds, from Friguia Park, Tunisia
+                //create a string array named myArrayOfAnimalData
+
+                String[] myArrayOfAnimalData = myFileLine.split(",");
+
+                String[] myArrayOfGenderSpecies = myArrayOfAnimalData[0].split(" ");
+
+                //output the age, gender and species
+
+               /* System.out.println("\n age in years: " + myArrayOfGenderSpecies[0]);
+                System.out.println("\n text for age(should be 'year') " + myArrayOfGenderSpecies[1]);
+                System.out.println("\n text for age (should be 'old') " + myArrayOfGenderSpecies[2]);
+                System.out.println("\n gender is " + myArrayOfGenderSpecies[3]);
+                System.out.println("\n species is " + myArrayOfGenderSpecies[4]);*/
+                System.out.println("\n" +myArrayOfGenderSpecies[4]+" "+ myArrayOfGenderSpecies[0]+" "+ myArrayOfGenderSpecies[1]+" "+ myArrayOfGenderSpecies[2]+" "+myArrayOfGenderSpecies[3]);
+
+                //code up the birthDate() method
+
+                //get todays date
+                Calendar calendar = Calendar.getInstance();
+                Date today = calendar.getTime();
+
+                LocalDate currentDate = LocalDate.now();
+                int year = currentDate.getYear();
+
+                //print the year
+                System.out.println("\nCurrent Year: " + year);
+
+                int animalsYearOfBirthDate = year - Integer.parseInt(myArrayOfGenderSpecies[0]);
+
+                //split the next group of words by a space
+                String[] myArrayOfBirthSeason = myArrayOfAnimalData[1].split(" ");
+
+                String birthSeason = myArrayOfBirthSeason[3];
+                System.out.println("\n birth season = " + birthSeason + "\n\n");
 
 
-        // create a Hyena object
+            //chat gpt recommendation:
+                int currentYear = LocalDate.now().getYear();
+                int age=0;
+                int yearOfBirth = currentYear - age;
+                String myAnimalBD = "";
+
+                if (birthSeason.contains("spring")) {
+                    myAnimalBD = "Mar 31, " + yearOfBirth;
+                } else if (birthSeason.contains("summer")) {
+                    myAnimalBD = "Jun 30, " + yearOfBirth;
+                } else if (birthSeason.contains("fall")) {
+                    myAnimalBD = "Sep 30, " + yearOfBirth;
+                } else if (birthSeason.contains("winter")) {
+                    myAnimalBD = "Dec 31, " + yearOfBirth;
+                } else {
+                    myAnimalBD = "Unknown";
+                }
 
 
-        Hyena myNewHyena = new Hyena();
 
-        System.out.println("\n Number of animals is " + myNewHyena.getNumOfAnimals() );
-        System.out.println("\n Number of hyenas: " + myNewHyena.getNumOfHyenas());
 
-        Hyena anotherHyena = new Hyena();
+                System.out.println("\n\n Animal birth date is: " + myAnimalBD + "\n\n");
 
-        System.out.println("\n Number of animals is " + myNewHyena.getNumOfAnimals() );
-        System.out.println("\n Number of hyenas: " + anotherHyena.getNumOfHyenas());
 
-        //how many hyenas
+                //subtract 4 years
+                calendar.add(Calendar.YEAR, - Integer.parseInt(myArrayOfGenderSpecies[0]));
+                //get the new date after subtraction
+                Date yearsAgo = calendar.getTime();
 
-        int currentNumOfHyenas = Hyena.getNumOfHyenas();
+                //print the original and new dates
 
-        System.out.println("\n the number of hyenas is: " + currentNumOfHyenas + "\n\n");
+                System.out.println("Today's Date: " + today);
+                System.out.println("Date: " + myArrayOfGenderSpecies[1] + " years ago " + yearsAgo);
 
-        Hyena oneMore = new Hyena();
+                System.out.println("\n Species is " + myArrayOfGenderSpecies[4]);
+                System.out.println("\n Animal Number " + myCounter + "*******");
+                System.out.println("\n myArrayOFAnimalData[0] is..." + myArrayOfAnimalData[0]);
+                System.out.println("\n myArrayOFAnimalData[1] is..." + myArrayOfAnimalData[1]);
+                System.out.println("\n myArrayOFAnimalData[2] is..." + myArrayOfAnimalData[2]);
+                System.out.println("\n myArrayOFAnimalData[3] is..." + myArrayOfAnimalData[3]);
+                System.out.println("\n myArrayOFAnimalData[4] is..." + myArrayOfAnimalData[4]);
+                System.out.println("\n myArrayOFAnimalData[5] is..." + myArrayOfAnimalData[5]);
+               
+                //increment the animal counter
+                myCounter++;
 
-        oneMore.setAnimalID("Hy09");
-        System.out.println("\n The ID of oneMore is: " + oneMore.getAnimalID() );
 
-        oneMore.setAnimalColor("brown stripes");
-        System.out.println("\n The color of oneMore is: " + oneMore.getAnimalColor());
 
-        String theNewID = genUniqueID("hyena", 8);
-        System.out.println("\n The new ID is: " + theNewID);
 
-    }
+
+            }
+        }
+
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+  }
 }
